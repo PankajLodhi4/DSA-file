4d8e8406a8d592e016a9b63fa9fc302e6d06cdcb
pankaj lodhi <lodhipankaj754@gmail.com>
2024-11-11 23:34:37 +0530

    factorial program

--- 0Basic.cpp
+++ 0Basic.cpp
@@ -5,9 +5,19 @@ using  namespace std;
 int main()
 {
 bool b = true;
-        std::cout<<"jai jai shri ram ";
-    char a = 123456;
+    cout<<"jai jai shri ram ";
+    // char a = 123456;
     cout<<a;
-    return 0;
+    //factorial calculate
+    int n = 4;
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+        }
+        cout<<"factorial of "<<n<<" is "<<fact;
+        return 0;
+        }
+       
 
-}
+   
