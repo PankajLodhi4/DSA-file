257881864deee915bb90b16025394c3f811c44e7
pankaj lodhi <lodhipankaj754@gmail.com>
2024-11-12 23:34:08 +0530

    patterns question

--- 0Basic.cpp
+++ 0Basic.cpp
@@ -4,20 +4,84 @@ using  namespace std;
 
 int main()
 {
-bool b = true;
-    cout<<"jai jai shri ram "<<"github";
-    // char a = 123456;
-    cout<<a;
+// bool b = true;
+//     cout<<"jai jai shri ram "<<"github";
+//     // char a = 123456;
+//     cout<<a;
     //factorial calculate
-    int n = 4;
-    int fact = 1;
-    for(int i = 1; i <= n; i++)
-    {
-        fact = fact * i;
-        }
-        cout<<"factorial of "<<n<<" is "<<fact;
-        return 0;
-        }
-       
+    // int n = 4;
+    // int fact = 1;
+    // for(int i = 1; i <= n; i++)
+    // {
+    //     fact = fact * i;
+    //     }
+    //     cout<<"factorial of "<<n<<" is "<<fact;
+      
+// ################################################################################
+    // Q : 1 2 3 4
+     //    1 2 3 4                           
+    //  int i =1;
+    //  int n ;
+    //  cout<<"Enter the number ";
+    //  cin>>n;
 
-   
+
+    //  while(i<=n){
+    //     int j =1;
+    //     while(j<=n){
+    //         cout<<n-j+1<<" ";
+    //         j = j+1;
+    //     }
+    //     cout<<endl;
+    //     i =i+1;
+
+    //  }
+
+//  ############################################################################  
+    // EX: 1 2 3
+    //     4 5 6
+    //     7 8 9
+//       int i =1;
+//      int count = 1;
+//      int n;
+//      cout<<"Enter the number ";
+//      cin>>n;
+//    while(i<=n){
+//        int j =1;
+//        while(j<=n){
+//        cout<<count<<" ";
+//        count = count +1;
+//          j = j+1;
+//        }
+//        cout<<endl;
+//        i = i+1;
+//    }
+
+
+
+
+
+
+
+//  ############################################################################  
+    // // EX: *
+    //        **
+    //        ***
+    //        ****
+      int i =1,n;
+     cout<<"Enter the number ";
+     cin>>n;
+   while(i<=n){
+       int j =1;
+       while(j<=i){
+    //    cout<<"*";
+     cout<<i;
+         j = j+1;
+       }
+       cout<<endl;
+       i = i+1;
+   }
+
+
+     return 0;
+        }
